7a60dab5131b0429533e30a35acabc24a05b8bfe
aman-d-1-n-only <abhitheone456@gmail.com>
2021-03-29 17:10:25 +0530

    feat : create a main enrty point button of plugin

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
 import joplin from 'api';
+import { MenuItemLocation } from 'api/types';
+
 
 joplin.plugins.register({
 	onStart: async function() {
-		console.info('Test plugin started!');
+		
+		/*******************Driver Code*******************/
+
+		//---------respective command for main button
+		await joplin.commands.register({
+            name: 'staticSiteExporterDialog',
+            label: 'Export to SSG',
+            execute: async (folderId: string) => {
+				console.log('Successfully created a entry point button and associated a command with it.')
+            },
+
+		});
+		
+		//---------created main button[entry point to wh]
+		await joplin.views.menuItems.create('Export to SSG', 'staticSiteExpoterDialog', MenuItemLocation.FolderContextMenu);
 	},
 });
